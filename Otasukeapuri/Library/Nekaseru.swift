5ba2ba3b07165299d3bde8176ee94f4e501f2bf2
Rakuda <rangudosya8@gmail.com>
2024-10-26 22:07:04 +0900

    フォント変更

--- Otasukeapuri/Library/Nekaseru.swift
+++ Otasukeapuri/Library/Nekaseru.swift
@@ -16,6 +16,21 @@ struct Nekaseru: View {
                     .resizable()
                     .ignoresSafeArea()
                     .scaledToFill()
+
+                VStack{
+                    Text("ここは思い悩んだアイデアと一旦距離を取るための場所です。一定期間時間アイデアを寝かせた後にもう一度アイデアを見た時、あなたがどのように感じるでしょうか。")
+                        .font(.custom("HannariMincho-Regular", size: 15))
+                        .foregroundColor(.white) // 文字色
+                        .frame(width: 300,height: 100)
+
+                        .padding(.top,90)
+
+                    Spacer()
+
+
+
+                }
+
             }
             // もどるボタン系
             .navigationBarBackButtonHidden(true)
